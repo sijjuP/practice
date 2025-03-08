86d5414a82d98265b6e5a7c27db6421bc538d5a4
SrinijaP <137266354+sijjuP@users.noreply.github.com>
2025-03-08 14:43:43 +0530

    Add files via upload

---
+++ Customexcption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Customexcption: Exception
+    {
+        public override string Message => $"{DateTime.Now.ToString()}: Internal Exception in custom";
+    }
+}
+
