c8f3b9a56d7e1fbdaa229cf7072c0daceedabd1e
SrinijaP <137266354+sijjuP@users.noreply.github.com>
2025-03-08 14:16:08 +0530

    Update Exceptioncls.cs

--- Exceptioncls.cs
+++ Exceptioncls.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +10,19 @@ namespace Practice
     {
         public static void exceptiontrial()
         {
+            int j = 5;
             int[] ar = { 3 };
             int k = Int32.MaxValue;
             try
-            { int i = ar[2]; 
+            {
+                if (5 == j)
+                    throw new Exception("no 5");
+                int i = ar[2]; 
               checked
                 {
                     k = k + 12;
                 }
+                
             }
             catch(IndexOutOfRangeException ex)
             {
@@ -28,6 +33,10 @@ namespace Practice
             {
                 Console.WriteLine(ex.Message);
                 //throw new ArgumentOutOfRangeException("not int");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             finally
             {
